46894a00ecd90595ed2c7a9dc2e8b50d44ac3002
Jefferyth-carvajalino <jefferyth2401@gmail.com>
2022-12-20 15:24:50 -0500

    adviser list

---
+++ resources/views/layouts/nuestrosAsesores.blade.php
@@ -0,0 +1,34 @@
+<section id="s-na">
+			<div class="advisers-list-component">
+	<div class="na-head">
+		<h2>Nuestros asesores</h2>
+		<ul class="na-tabs">
+			<li>
+				<a href="#" class="active">
+					Destacados
+				</a>
+			</li>
+		</ul>
+		<div class="na-sbox-container">
+			<form id="search-adviser-form">
+				<button type="submit" id="btn-search">
+					<span>
+						<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+							<path fill="#00e5e5" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"></path>
+						</svg>
+					</span>
+					<span>
+						buscar
+					</span>
+				</button>
+				<input type="text" id="search-adviser-input">
+			</form>
+		</div>
+	</div>
+	<div class="na-body" id="advisers-box">
+		</div>
+	<div class="paginator-container"><div class="paginationjs paginationjs-theme-blue"><div class="paginationjs-pages"><ul><li class="paginationjs-prev disabled"><a>«</a></li><li class="paginationjs-page J-paginationjs-page active" data-num="1"><a>1</a></li><li class="paginationjs-page J-paginationjs-page" data-num="2"><a href="">2</a></li><li class="paginationjs-page J-paginationjs-page" data-num="3"><a href="">3</a></li><li class="paginationjs-page J-paginationjs-page" data-num="4"><a href="">4</a></li><li class="paginationjs-next J-paginationjs-next" data-num="2" title="Next page"><a href="">»</a></li></ul></div></div></div>
+	<div id="content-btn-trabaja-con-nosotros" style="display: block;">
+		<a href="/trabaja-con-nosotros/">Trabaja con nosotros</a>
+	</div>
+</div>		</section>
